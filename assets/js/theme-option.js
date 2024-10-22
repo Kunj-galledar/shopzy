b85c544e737a66fca26c9b20ad1f27e7f9ddb803
Kunj Galledar <galledarkunj8@gmail.com>
2024-10-23 02:03:53 +0530

    Update
    checked dark and rtl on all pages.
    added page list.

--- assets/js/theme-option.js
+++ assets/js/theme-option.js
@@ -48,37 +48,37 @@
 
 
 
-// const toggleSwitch = document.getElementById('dark');
-// const body = document.body;
+const toggleSwitch = document.getElementById('dark');
+const body = document.body;
 
-// // Check localStorage for the dark mode setting
-// const darkMode = localStorage.getItem('darkMode');
+// Check localStorage for the dark mode setting
+const darkMode = localStorage.getItem('darkMode');
 
-// if (darkMode === 'enabled') {
-//     body.classList.add('dark-mode');
-//     toggleSwitch.checked = true;
-// }
+if (darkMode === 'enabled') {
+    body.classList.add('dark-mode');
+    toggleSwitch.checked = true;
+}
 
-// toggleSwitch.addEventListener('change', function () {
-//     if (toggleSwitch.checked) {
-//         body.classList.add('dark-mode');
-//         localStorage.setItem('darkMode', 'enabled');
-//     } else {
-//         body.classList.remove('dark-mode');
-//         localStorage.setItem('darkMode', 'disabled');
-//     }
-// });
+toggleSwitch.addEventListener('change', function () {
+    if (toggleSwitch.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
 
-// document.getElementById('rtl').addEventListener('change', function () {
-//     // Get the current stylesheet
-//     const stylesheet = document.querySelector('link[rel="stylesheet"]');
+document.getElementById('rtl').addEventListener('change', function () {
+    // Get the current stylesheet
+    const stylesheet = document.querySelector('link[rel="stylesheet"]');
 
-//     // Toggle between two stylesheets and apply RTL or LTR direction based on the checkbox state
-//     if (this.checked) {
-//         stylesheet.setAttribute('href', './assets/css/vendor/bootstrap.rtl.css');
-//         document.body.dir = 'rtl'; // Set body direction to RTL
-//     } else {
-//         stylesheet.setAttribute('href', './assets/css/vendor/bootstrap.css');
-//         document.body.dir = 'ltr'; // Set body direction to LTR
-//     }
-// });
+    // Toggle between two stylesheets and apply RTL or LTR direction based on the checkbox state
+    if (this.checked) {
+        stylesheet.setAttribute('href', './assets/css/vendor/bootstrap.rtl.css');
+        document.body.dir = 'rtl'; // Set body direction to RTL
+    } else {
+        stylesheet.setAttribute('href', './assets/css/vendor/bootstrap.css');
+        document.body.dir = 'ltr'; // Set body direction to LTR
+    }
+});
