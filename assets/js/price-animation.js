a4ee9bd5ff925e19ea1df2c742b56e96b06a4b0b
suchikaparekh <48489158+suchikaparekh@users.noreply.github.com>
2024-10-24 18:41:09 +0530

    design changes

---
+++ assets/js/price-animation.js
@@ -0,0 +1,8 @@
+/*=====================
+    Price Animation Js
+==========================*/
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector('.price-wrapper').classList.add('show');
+    }, 1000);
+});
